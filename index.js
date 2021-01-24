98032238376f7703efb5ad90f0eabd4a8f459567
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-01-24 15:20:46 +0100

    animation adde

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHtml = this.innerHTML;
     makeSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
   });
 }
 
 // Detecting keyboard Press
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -51,4 +53,14 @@ function makeSound(key) {
 
     default:
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
